29b0513c50382b9457556aca040f5f5703d78c87
Adam Armour <adam.armour@gmail.com>
2015-11-05 16:44:47 -0700

    docs(demo): add code examples

--- demo.js
+++ demo.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import hljs from 'highlight.js';
 import styles from './demo.css';
 import { CourseList, CourseListItem } from './src/course';
+
+hljs.initHighlightingOnLoad();
 
 function handleAction(action, ...data) {
   console.log('%s: %s', action, JSON.stringify(data));
 }
+
+const CodeExample = React.createClass({
+
+  propTypes: {
+    code: PropTypes.string.isRequired
+  },
+
+  getInitialState() {
+    return { visible: false };
+  },
+
+  handleToggleVisible(event) {
+    this.setState({ visible: !this.state.visible });
+    event.preventDefault();
+  },
+
+  render() {
+    const visible = { display: this.state.visible ? 'block' : 'none' };
+
+    return (
+      <div>
+        <a href="#" onClick={this.handleToggleVisible}>{this.state.visible ? 'Hide' : 'Show'} code</a>
+        <div className={styles.code} ref="code" style={visible}>
+          <pre><code>{this.props.code}</code></pre>
+        </div>
+      </div>
+    );
+  }
+
+});
 
 function renderCourseListItem() {
   const educators = ['Sigmund Freud', 'B. F. Skinner']
@@ -13,20 +46,37 @@ function renderCourseListItem() {
   const endDate = new Date('12-31-2018');
   const thumbnailUrl = 'http://www.fillmurray.com/g/300/200';
 
+  const code = `
+    <CourseListItem
+      id='psychology-101'
+      title="Psychology 101"
+      educators=['Sigmund Freud', 'B. F. Skinner']
+      startDate={new Date('04-02-1980')}
+      endDate={new Date('12-31-2018')}
+      thumbnailUrl="http://www.fillmurray.com/g/300/200"
+      onSelectCourse={handleSelectCourse}
+      onRemoveCourse={handleRemoveCourse}
+      onInviteStudents={handleInviteStudents}
+      onCourseSettings={handleCourseSettings}
+    />
+  `;
+
   return (
     <div>
       <h3>Course List Item</h3>
       <CourseListItem
-        id='psychology-101'
+        id="psychology-101"
         title="Psychology 101"
         educators={educators}
         startDate={startDate}
         endDate={endDate}
         thumbnailUrl={thumbnailUrl}
         onSelectCourse={handleAction.bind(null, 'select course')}
+        onRemoveCourse={handleAction.bind(null, 'remove course')}
         onInviteStudents={handleAction.bind(null, 'invite students')}
         onCourseSettings={handleAction.bind(null, 'course settings')}
       />
+      <CodeExample code={code} />
     </div>
   );
 }
@@ -61,6 +111,16 @@ function renderCourseList() {
     }
   ];
 
+  const code = `
+    <CourseList
+      courses=[...]
+      onSelectCourse={handleSelectCourse}
+      onRemoveCourse={handleRemoveCourse}
+      onInviteStudents={handleInviteStudents}
+      onCourseSettings={handleCourseSettings}
+    />
+  `;
+
   return (
     <div>
       <h3>Course List</h3>
@@ -71,11 +131,12 @@ function renderCourseList() {
         onInviteStudents={handleAction.bind(null, 'invite students')}
         onCourseSettings={handleAction.bind(null, 'course settings')}
       />
+      <CodeExample code={code} />
     </div>
   );
 }
 
-const Demo = (props) => {
+function Demo(props) {
   return (
     <div>
       {renderCourseListItem()}
@@ -83,6 +144,6 @@ const Demo = (props) => {
       {renderCourseList()}
     </div>
   );
-};
+}
 
 ReactDOM.render(<Demo />, document.getElementById('root'));
